7239a1cb82e16d8705c9ec6b795b9b138d2af669
manuelrobertomunoz <mmroberto100@gmail.com>
2024-10-25 04:05:37 -0700

    Kotlin Coroutines & Flows Coroutine Basics
    [Easy] Assignment #1

--- app/src/main/java/com/mmunoz/coroutinesbasics/MainActivity.kt
+++ app/src/main/java/com/mmunoz/coroutinesbasics/MainActivity.kt
@@ -4,32 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.mmunoz.coroutinesbasics.ui.theme.CoroutinesBasicsTheme
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        GlobalScope.launch {
-            launch {
-                launch {
-                    delay(1000L)
-                    println("Innermost coroutine finished")
-                }
-                delay(500L)
-                println("Middle coroutine finished")
+
+        fun CoroutineScope.sound(pace: Long, sound: String) = launch {
+            repeat(4) { time ->
+                delay(pace)
+                println("$sound at time ${time + 1} after ${pace / 1000} seconds")
             }
-            println("Outer coroutine finished")
+        }
+
+        lifecycleScope.launch {
+
+            val bird1 = sound(1000, "Coo")
+            val bird2 = sound(2000, "Caw")
+            val bird3 = sound(3000, "Chirp")
+
+            val timeMillis = measureTimeMillis {
+                bird1.join()
+                bird2.join()
+                bird3.join()
+            }
+
+            println("Time of execution: $timeMillis")
         }
 
         setContent {
